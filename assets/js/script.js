cc9b10fae9a7602dc124168650afcee016e1f421
Neha <neha@Nehas-MacBook-Air.local>
2025-05-27 13:34:30 +0530

    completed static version

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,17 @@ function menuIcon() {
     document.querySelector('.menu-list').classList.toggle('show');
     document.querySelector('nav').classList.toggle('activeNavBar');
 
-}
+};
+
+
+const headers = document.querySelectorAll('.accordion-header');
+
+headers.forEach(header => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling;
+        const arrow = header.querySelector('.accordion-arrow');
+
+        content.classList.toggle('open');
+        arrow.classList.toggle('rotate');
+    });
+});
