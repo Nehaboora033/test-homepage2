33837e87d92081e45bce9a24775e751d5fc18859
Neha <neha@Nehas-MacBook-Air.local>
2025-05-27 10:57:53 +0530

    completed nav reaponsive

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function menuIcon() {
+    document.querySelector('.menu-list').classList.toggle('show');
+    document.querySelector('nav').classList.toggle('activeNavBar');
+
+}
+console.log(menuIcon());
